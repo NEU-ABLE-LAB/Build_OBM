dcfcaf40c6550cec3940bb65fd950305dd3a7625
Kunind <Kunind.k@northeastern.edu>
2022-09-07 19:16:23 -0400

    Improved readability

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from tools import *
 # Occupant agent class
 class Occupant(mesa.Agent):
     """ An occupant agent: Contains information specific to an occupant """
-    def __init__(self, unique_id: int, model, home_ID, model_class, model_regres) -> None:
+    def __init__(self, unique_id: int, model, home_ID, TM_occupancy , TM_habitual, model_class, model_regres) -> None:
         super().__init__(unique_id, model)
         # Occupant's residence
         self.home_ID = home_ID
@@ -17,27 +17,36 @@ class Occupant(mesa.Agent):
         # Flag to show if the change in setpoint was implemented by the occupant
         self.recent_stp_change = False
         # Occupancy Model - Transition matrix to simulate occupant's presence
-        self.occupancy_tp_matrix = pd.read_csv('data/transition_matrix.csv')
+        self.occupancy_tp_matrix = TM_occupancy
         # Occupancy Model - Place holder variable to contain simulated occupancy for a simulation's day
         self.occupancy = []
-        # Comfort model - tracking setpoints
-        self.setpoints = {'current': {'heat':None, 'cool': None},\
-             'previous':{'heat':None, 'cool': None}}
+        # # Comfort model - tracking setpoints
+        # self.setpoints = {'current': {'heat':None, 'cool': None},\
+        #      'previous':{'heat':None, 'cool': None}}
         # Track comfort temperature
         self.T_CT = 72.0 # Static value for now
         # Time to override value container
         self.TTO = None
 
         # Habitual model - Transition matrix 
-        self.habitual_tp_matrix = pd.read_csv('TM_habitual.csv')
+        self.habitual_tp_matrix = TM_habitual
         self.habitual_schedule = []
-        self.del_T_MSC = 0
         self.discomfort_class_model = model_class
         self.discomfort_regres_model = model_regres
+
+        # Simulation output container
+        self.output = {'T_stp_cool':None, 'T_stp_heat':None}
 
         print(f"Occupant created, ID: {unique_id}")
 
     def step(self) -> None:
+        T_stp_cool, T_stp_heat = (self.current_env_features['T_stp_cool'], self.current_env_features['T_stp_heat'])
+
+        """ 
+        +-------------------+
+        | Model predictions |
+        +-------------------+
+        """
 
         # Synthesize occupancy data
         if self.model.schedule.steps % 24 == 0:
@@ -50,79 +59,48 @@ class Occupant(mesa.Agent):
             self.habitual_schedule.extend(Markov_habitual_model(self.habitual_tp_matrix, sampling_time = self.model.sampling_frequency))
         
         # Comfort Model: Predict comfortable temperature
-        if self.model.schedule.steps == 0:
-            # No prior data exists, assume previous and current data to be same
-            self.setpoints['previous']['heat'] = self.current_env_features['T_stp_heat']
-            self.setpoints['previous']['cool'] = self.current_env_features['T_stp_cool']
-        else:
-            # Update data
-            self.setpoints['previous']['heat'] = self.setpoints['current']['heat']
-            self.setpoints['previous']['cool'] = self.setpoints['current']['cool']
-
-        self.setpoints['current']['heat'] = self.current_env_features['T_stp_heat']
-        self.setpoints['current']['cool'] = self.current_env_features['T_stp_cool']
-        
-        # Define change in setpoint
-        T_stp_heat_change = self.setpoints['current']['heat'] - self.setpoints['previous']['heat']
-        T_stp_cool_change = self.setpoints['current']['cool'] - self.setpoints['previous']['cool']
-
-        # Define the comfort temperature if:
-        #   1. The previous change was not incurred by the occupant
-        #   2. There was setpoint change
-        if ( abs(T_stp_cool_change) != 0 or abs(T_stp_heat_change) != 0 ) and not self.recent_stp_change:
-            
-            # Find the type of setpoint change
-            if T_stp_cool_change != 0:
-                cooling_stp_change = True
-            if T_stp_heat_change != 0:
-                heating_stp_change = True
-            
-            # If both type of setpoints were changed then refer to largest setpoint change as the primary setpoint change
-            # TODO: Develop this methodology to find the type of setpoint change. 
-            if cooling_stp_change and heating_stp_change:
-                if abs(cooling_stp_change) > abs(heating_stp_change):
-                    heating_stp_change = False
-                else:
-                    cooling_stp_change = False
-      
-            if not self.recent_stp_change:
-                self.T_CT = np.mean([self.setpoints['previous']['heat'], self.setpoints['previous']['cool']])
-            else:
-                self.T_CT = 72 # TODO: If the thermostat schedule was overriden then assign that value
+        # TODO: Dynamic model to be placed here
 
         # Discomfort Model:
-        if self.TTO is not None:
+        if self.TTO is None:
+            # Decrease the timer per timestep if a TTO value exists
+            if self.TTO == 0:
+                raise ValueError('Check TTO')
             if self.TTO:
                 self.TTO =- 1
-
-            ['T_ctrl', 'T_stp_cool', 'T_stp_heat', 'hum', 'T_out', 'fan', 'mo', 'equip_run_heat', 'equip_run_cool']
-            df = self.current_env_features
-            df_class = df[['T_ctrl', 'T_stp_cool', 'T_stp_heat', 'hum', 'T_out', 'fan', 'mo', 'equip_run_heat', 'equip_run_cool']].copy()
-            predictions = self.discomfort_class_model.predict(df_class.values.reshape(1,-1))
             
-            if predictions[0]:
-                df_regress = df.copy()
-                df_regress = df_regress.drop(['DateTime','schedule', 'event'])
+            # Classify override using the random forest classification model
+            is_override = self.discomfort_class_model.predict([list(self.current_env_features.values())])[0]
+            
+            if is_override:
+                # Estimate time to override for the classified override using the random forest regressor model
+                self.TTO = int(np.round(self.discomfort_regres_model.predict([list(self.current_env_features.values())])))
 
-                df_regress= df_regress[['T_ctrl','T_stp_cool','T_stp_heat','hum','T_out','fan','mo','equip_run_heat','equip_run_cool','year','day','hour','day_of_week']]
-                features = np.array(df_regress)
+        """ 
+        +---------------------------+
+        | Override decision process |
+        +---------------------------+
+        """
+        # If routine based habitual model predicts override and the occupant is present in the home:
+        #  then decide the setpoint change
+        if self.habitual_schedule[self.model.timestep_day] and self.occupancy[self.model.timestep_day]:
+        
+            T_stp_cool, T_stp_heat = decide_heat_cool_stp(self.T_CT, self.current_env_features['T_in'],\
+                 self.current_env_features['T_stp_heat'], self.current_env_features['T_stp_cool'])
 
-                self.TTO = np.round(self.discomfort_regres_model.predict(features.reshape(1,-1)))
-
-        # Override decision based on occupancy
-        if self.habitual_schedule[self.model.timestep_day] == 1 and self.occupancy[self.model.timestep_day] == 1:
-            implement_heat_cool_override(occupant=self,df=df)
-
-        if self.TTO == 0:
-            implement_heat_cool_override(occupant=self,df=df)
+        if self.TTO == 0 and self.occupancy[self.model.timestep_day]:
+            T_stp_cool, T_stp_heat = decide_heat_cool_stp(self.occupant.T_CT, self.current_env_features['T_in'],\
+                 self.current_env_features['T_stp_heat'], self.current_env_features['T_stp_cool'])
             self.TTO = None
 
+        self.output['T_stp_cool'], self.output['T_stp_heat'] = T_stp_cool, T_stp_heat
 
-        print(f"Occupant ID: {self.unique_id} simulated")
+
+        print(f"Occupant idN: {self.unique_id} simulated")
 
 class OccupantModel(mesa.Model):
     """ Occupant model: Contains model simulation information required """
-    def __init__(self, N_homes,N_occupants_in_home, sampling_frequency, model_class, model_regres) -> None:
+    def __init__(self, N_homes,N_occupants_in_home, sampling_frequency, TM_occupancy, TM_habitual, model_class, model_regres) -> None:
         super().__init__()
         # Number of occupants to be simulated in a home
         self.N_occupants_in_home = N_occupants_in_home
@@ -138,27 +116,25 @@ class OccupantModel(mesa.Model):
         self.current_min_of_the_day = 0
         # Simulation's timestep equivalent day of the week
         self.current_day_of_the_week = 0
-        # 
+        # Simulation's equivalent of timestep of the day (for 5-min sampling frequency, max value of this var is 288)
         self.timestep_day = 0
-        #
-        self.stp_cool = None
-        #
-        self.stp_heat = None
 
         # Create homes
         for home_ID in range(0, N_homes):
             for occup_ID in range(0,self.N_occupants_in_home):
                 # Create occupant
-                occup = Occupant(unique_id=home_ID + occup_ID, model=self, home_ID=home_ID, model_class = model_class, model_regres=model_regres)
+                occup = Occupant(unique_id=home_ID + occup_ID, model=self, home_ID=home_ID, \
+                    TM_occupancy = TM_occupancy, TM_habitual = TM_habitual, \
+                        model_class = model_class, model_regres=model_regres)
                 # Add occupant to the scheduler
                 self.schedule.add(occup)
 
-    def step(self, data) -> None:
+    def step(self, ip_data_env) -> None:
         print(f"Time step: {self.schedule.steps}")
 
         # Send simulated indoor env data to the occupant agent
         for agent in self.schedule.agents:
-            agent.current_env_features = data.loc[self.schedule.steps].copy()
+            agent.current_env_features = ip_data_env
 
         self.schedule.step()
 
--- run.py
+++ run.py
@@ -5,31 +5,44 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Read a local dataframe to simulate the indoor environment conditions
+# Read a local files
 df = pd.read_hdf('sample_data1_stp_processed.h5')
-df.rename({'T_ctrl':'T_in'}, axis = 1)
+TM_occupancy = pd.read_csv('data/transition_matrix.csv')
+TM_habitual = pd.read_csv('TM_habitual.csv')
 model_regressor = pickle.load(open('model_regressor.pkl','rb'))
 model_classification = pickle.load(open('model_classification.pkl','rb'))
-t = []
-T_SP = []
 
-sim_sampling_interval = 5 # minutes -> cannot exceed this interval as the TM's are not available for a lower frequency
+t = []
+T_stp_heat = []
+T_stp_cool = []
+
+sim_sampling_frequency = 5 # minutes -> cannot exceed this interval as the TM's are not available for a lower frequency
 # TODO: To add raise value error if the value is higher thatn 5 minutes
 # Initiate OccupantModel
 Occup_model = OccupantModel(N_homes= 1, N_occupants_in_home=1,
-                            sampling_frequency=sim_sampling_interval,
+                            sampling_frequency=sim_sampling_frequency,
+                            TM_occupancy = TM_occupancy, TM_habitual = TM_habitual,
                             model_class = model_classification,
                             model_regres=model_regressor )
 
 # Simulate the occupant model for specific timesteps
-for timeStepN in range(0,int(1440/sim_sampling_interval)):
-    t.append(timeStepN)
-    Occup_model.step(df)
+for timeStepN in range(0,int((1440)/sim_sampling_frequency)):
+    t.append(timeStepN)  # For plotting
+
+    Occup_model.step(ip_data_env= {'T_in':df.loc[timeStepN,'T_ctrl'],
+                            'T_stp_cool':df.loc[timeStepN,'T_stp_cool'],
+                            'T_stp_heat':df.loc[timeStepN,'T_stp_heat'],
+                            'hum':df.loc[timeStepN,'hum'],
+                            'T_out':df.loc[timeStepN,'T_out'],
+                            'mo':df.loc[timeStepN,'mo'],
+                            'equip_run_heat':df.loc[timeStepN,'equip_run_heat'],
+                            'equip_run_cool':df.loc[timeStepN,'equip_run_cool']
+                            })
     for agents in Occup_model.schedule.agents:
-        if agents.del_T_MSC != 0:
-            T_SP.append(agents.agents.del_T_MSC)
+        T_stp_heat.append(agents.output['T_stp_heat'])
+        T_stp_cool.append(agents.output['T_stp_cool'])
 
 # plt.figure()
-# sns.lineplot(x=t,y=del_T_MSC)
+# sns.lineplot(x=t,y=T_stp_heat)
+# sns.lineplot(x=t,y=T_stp_cool)
 # plt.show()
-1+1
